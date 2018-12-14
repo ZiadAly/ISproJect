b9302f0a70249ce190f1b76d12c3db4b04e20122
MohamedSaeed08 <mohamedsaeed8917@gmail.com>
2018-12-14 21:25:33 +0200

    Update Control.php

--- ISProj/Control.php
+++ ISProj/Control.php
@@ -1,13 +1,41 @@
+<?php 
+include 'template.php';
 
+function content(){
 
+    $tok = $_SESSION['token'];
+    $k = $GLOBALS['key'];
+    $result ="";
+    try{
+    $decoded =  JWT::decode($tok, $k, array('HS256'));
+    $decoded_tok = (array) $decoded;
+    $id = $decoded_tok['uid'];
+    $role = $decoded_tok['role'];
 
+    }
+    catch(Exception $e){
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
 
+?>
 
-<button type='button' onclick="goto1()"> Student </button>
-<button type='button' onclick="goto2()"> TA </button>
-<button type='button' onclick="goto3()"> Doctor </button>
-
-
+<?php 
+if ($role == 'Student'){ ?>
+    <h4> Welcome, Click below to enter your page.</h4> 
+    <button type='button' onclick="goto1()"> click me ya Student </button>
+<?php }
+elseif ($role == 'TA') { ?>
+    <h4> Welcome, Click below to enter your page.</h4> 
+    <button type='button' onclick="goto2()"> click me ya TA </button>
+<?php } 
+elseif ($role == 'DR'){ ?> 
+    <h4> Welcome, Click below to enter your page.</h4>   
+    <button type='button' onclick="goto3()"> click me ya Doctor </button>
+<?php }
+else{
+    echo 'invalid user';
+}
+?>
 <script>
 function goto1(){
     window.location.replace("Student.php");
@@ -22,5 +50,5 @@ function goto3(){
     window.location.replace("Doctor.php");
 }
 
-
+<?php } ?>
 </script>
