6f4c8d5face40fae9dc751297455eb074122d529
ziad <zerobarca97@gmail.com>
2018-12-14 21:04:25 +0200

    k

---
+++ ISProj/Control.php
@@ -0,0 +1,26 @@
+
+
+
+
+
+<button type='button' onclick="goto1()"> Student </button>
+<button type='button' onclick="goto2()"> TA </button>
+<button type='button' onclick="goto3()"> Doctor </button>
+
+
+<script>
+function goto1(){
+    window.location.replace("Student.php");
+
+}
+
+function goto2(){
+    window.location.replace("TA.php");
+}
+
+function goto3(){
+    window.location.replace("Doctor.php");
+}
+
+
+</script>
---
+++ ISProj/Student.php
@@ -0,0 +1,61 @@
+<?php
+include 'template.php';
+function content(){
+
+    $t = $_SESSION['token'];
+    $k = $GLOBALS['key'];
+    $result ="";
+    try{
+    $decoded =  JWT::decode($t, $k, array('HS256'));
+    $decoded_tok = (array) $decoded;
+    $id = $decoded_tok['uid'];
+    
+   $result = mysqli_query($GLOBALS['connection'], " SELECT * FROM `grades` where `id` = '$id' ");
+        }
+    catch(Exception $e){
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    }
+  ?>
+  <table>
+  <tr>
+    <th>Id</th>
+    <th>Course</th>
+    <th>Grades</th>
+    <tbody>
+         <?php
+         while($row = mysqli_fetch_array($result)){
+         ?>
+             <tr>
+                 <td><?php echo $row['id']; ?></td>
+                 <td><?php echo $row['course']; ?></td>
+                 <td><?php echo $row['grade']; ?></td>
+                 <td><button type='button' onclick="edit()"> edit info </button></td>
+            <?php echo "<tr>";
+          }
+               ?>
+          </tbody>
+</table>
+
+<?php }?>
+
+
+<style>
+table, td, th {
+  border: 1px solid black;
+}
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+},
+th {
+  height: 50px;
+}
+</style>
+
+<script type="text/javascript">
+function edit(){
+  window.location.replace("index.php");
+}
+
+</script>
